9462f7f9541f78c85dcf5b5ef04a880cb35555ff
smarthug <kirklayer@gmail.com>
2024-10-19 08:12:24 -0700

    feat: integrate Dynamic Labs SDK for wallet connection and dynamic widget

--- baldr-frontend/src/App.jsx
+++ baldr-frontend/src/App.jsx
@@ -5,19 +5,30 @@ import EscrowTradePage from "./pages/EscrowTrade";
 import EscrowCreatePage from "./pages/EscrowCreate";
 import InventoryPage from "./pages/Inventory";
 import HistoryPage from "./pages/History";
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTE_PATH.MAIN} element={<EscrowListPage />}></Route>
-        <Route path={ROUTE_PATH.ESCROW_LIST} element={<EscrowListPage />}></Route>
-        <Route path={ROUTE_PATH.ESCROW_TRADE} element={<EscrowTradePage />}></Route>
-        <Route path={ROUTE_PATH.ESCROW_CREATE} element={<EscrowCreatePage />}></Route>
-        <Route path={ROUTE_PATH.INVENTORY} element={<InventoryPage />}></Route>
-        <Route path={ROUTE_PATH.HISTORY} element={<HistoryPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <DynamicContextProvider
+      settings={{
+        environmentId: '9ebc2da2-5d7d-4c1e-85f8-6442ff43725c',
+        walletConnectors: [EthereumWalletConnectors],
+      }}>
+
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTE_PATH.MAIN} element={<EscrowListPage />}></Route>
+          <Route path={ROUTE_PATH.ESCROW_LIST} element={<EscrowListPage />}></Route>
+          <Route path={ROUTE_PATH.ESCROW_TRADE} element={<EscrowTradePage />}></Route>
+          <Route path={ROUTE_PATH.ESCROW_CREATE} element={<EscrowCreatePage />}></Route>
+          <Route path={ROUTE_PATH.INVENTORY} element={<InventoryPage />}></Route>
+          <Route path={ROUTE_PATH.HISTORY} element={<HistoryPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </DynamicContextProvider>
   );
 }
 
