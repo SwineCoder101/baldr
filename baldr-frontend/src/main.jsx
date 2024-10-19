fcbb0ef79c4baddbb7167f657401b88892d33208
edac99 <jamjam1208@naver.com>
2024-10-18 23:16:10 -0700

    feat: set global style

--- baldr-frontend/src/main.jsx
+++ baldr-frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "styled-components";
 import theme from "../src/styles/theme.js";
+import { GlobalStyle } from "./styles/global-style.js";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>
 );
---
+++ baldr-frontend/src/styles/global-style.js
@@ -0,0 +1,98 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const CustomClass = `
+  .f-row {
+    display: flex;
+    flex-direction: row;
+  }
+  
+  .f-col {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .f-spb {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .small-pd {
+    padding: 1rem;
+  }
+
+  .big-pd {
+    padding: 1.8rem;
+  }
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  ${CustomClass}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    width: 100vw;
+    height: 100vh;
+    font-size: 80%;
+  }
+
+  body, #root {
+    width: 100%;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .scroll::-webkit-scrollbar {
+    display: none;
+  }
+
+  .scroll {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+
+  input {
+    outline: none;
+    border: none;
+  }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+
+  select::-ms-expand {
+    display: none;
+  }
+
+  // For mobile view
+  @media (max-width: 768px) {
+    html {
+      width: 100%;
+    }
+
+    body, #root {
+      width: 100%;
+    }
+  }
+`;
