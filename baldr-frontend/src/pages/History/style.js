8497d482c55932790ca120c3a961b334208a2dd1
edac99 <jamjam1208@naver.com>
2024-10-19 13:25:36 -0700

    chore: set history page wip

--- baldr-frontend/src/pages/History/style.js
+++ baldr-frontend/src/pages/History/style.js
@@ -4,4 +4,8 @@ import theme from "../../styles/theme";
 export const Wrapper = styled.div`
   background-color: ${theme.color.MAIN_BLACK};
   color: ${theme.color.WHITE};
+  font-size: ${theme.font.SIZE.ML};
+  font-weight: 600;
+  height: 100vh;
+  padding: 8rem 2rem 2rem 2rem;
 `;
