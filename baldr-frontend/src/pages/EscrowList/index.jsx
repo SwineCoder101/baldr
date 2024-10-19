f4cd37f19cf7be3cbcafbfa37b250b5f2abd3c56
edac99 <jamjam1208@naver.com>
2024-10-18 23:04:44 -0700

    feat: set global style (color, font-size)

--- baldr-frontend/src/pages/EscrowList/index.jsx
+++ baldr-frontend/src/pages/EscrowList/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_PATH } from "../../common/const";
+import { Wrapper } from "./style";
 
 const EscrowListPage = () => {
   const navigate = useNavigate();
@@ -9,7 +10,7 @@ const EscrowListPage = () => {
     navigate(ROUTE_PATH.ESCROW_LIST, { replace: true });
   }, [navigate]);
 
-  return <div>EscrowListPage</div>;
+  return <Wrapper>EscrowListPage</Wrapper>;
 };
 
 export default EscrowListPage;
