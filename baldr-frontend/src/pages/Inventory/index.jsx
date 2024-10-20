ba912e43a56ec7c33d2eb3f12611cad01a28e6d8
smarthug <kirklayer@gmail.com>
2024-10-20 02:47:14 -0700

    story init

--- baldr-frontend/src/pages/Inventory/index.jsx
+++ baldr-frontend/src/pages/Inventory/index.jsx
@@ -22,6 +22,10 @@ import Item6 from "../../assets/image/item6.png";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_PATH } from "../../common/const";
 
+import { StoryClient, StoryConfig } from '@story-protocol/core-sdk'
+import { http } from 'viem'
+import { account, RPCProviderUrl } from '../../utils'
+
 const items = [
   { name: "Item1", image: Item1 },
   { name: "Item2", image: Item2 },
@@ -48,10 +52,81 @@ const InventoryPage = () => {
   const handleMenuItemClick = (action) => {
     if (action === "Escrow") {
       navigate(ROUTE_PATH.ESCROW_CREATE);
+    } else if (action === "ProtectIP") {
+      console.log("IP Protected using Story network");
+      console.log(selectedItem)
+      // protectIP();
+
     } else {
       alert("Not yet");
     }
   };
+
+  const protectIP = async function () {
+    // 1. Set up your Story Config
+    //
+    // Docs: https://docs.story.foundation/docs/typescript-sdk-setup
+    const config = {
+        account: account,
+        transport: http(RPCProviderUrl),
+        chainId: 'iliad',
+    }
+    const client = StoryClient.newClient(config)
+
+    // 2. Set up your IP Metadata
+    //
+    // Docs: https://docs.story.foundation/docs/ipa-metadata-standard
+    const ipMetadata = client.ipAsset.generateIpMetadata({
+        title: 'My IP Asset',
+        description: 'This is a test IP asset',
+        attributes: [
+            {
+                key: 'Rarity',
+                value: 'Legendary',
+            },
+        ],
+    })
+
+    // 3. Set up your NFT Metadata
+    //
+    // Docs: https://eips.ethereum.org/EIPS/eip-721
+    const nftMetadata = {
+        name: 'NFT representing ownership of IP Asset',
+        description: 'This NFT represents ownership of an IP Asset',
+        image: 'https://i.imgur.com/gb59b2S.png',
+    }
+
+    // 4. Upload your IP and NFT Metadata to IPFS
+    const ipIpfsHash = await uploadJSONToIPFS(ipMetadata)
+    const ipHash = createHash('sha256').update(JSON.stringify(ipMetadata)).digest('hex')
+    const nftIpfsHash = await uploadJSONToIPFS(nftMetadata)
+    const nftHash = createHash('sha256').update(JSON.stringify(nftMetadata)).digest('hex')
+
+    // 5. Mint an NFT
+    const tokenId = await mintNFT(account.address, `https://ipfs.io/ipfs/${nftIpfsHash}`)
+    console.log(`NFT minted with tokenId ${tokenId}`)
+
+    // 6. Register an IP Asset
+    //
+    // Docs: https://docs.story.foundation/docs/register-an-nft-as-an-ip-asset
+    const response = await client.ipAsset.registerIpAndAttachPilTerms({
+        nftContract: NFTContractAddress,
+        tokenId: tokenId,
+        pilType: PIL_TYPE.NON_COMMERCIAL_REMIX,
+        mintingFee: 0, // empty - doesn't apply
+        currency: AddressZero, // empty - doesn't apply
+        ipMetadata: {
+            ipMetadataURI: `https://ipfs.io/ipfs/${ipIpfsHash}`,
+            ipMetadataHash: `0x${ipHash}`,
+            nftMetadataURI: `https://ipfs.io/ipfs/${nftIpfsHash}`,
+            nftMetadataHash: `0x${nftHash}`,
+        },
+        txOptions: { waitForTransaction: true },
+    })
+    console.log(`Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}`)
+    console.log(`View on the explorer: https://explorer.story.foundation/ipa/${response.ipId}`)
+}
+
 
   return (
     <>
@@ -81,6 +156,7 @@ const InventoryPage = () => {
                     <MenuItem onClick={() => handleMenuItemClick("Rental")}>Rental</MenuItem>
                     <MenuItem onClick={() => handleMenuItemClick("Lending")}>Lending</MenuItem>
                     <MenuItem onClick={() => handleMenuItemClick("Auction")}>Auction</MenuItem>
+                    <MenuItem onClick={() => handleMenuItemClick("ProtectIP")}>Protect IP</MenuItem>
                   </DropdownMenu>
                 )}
               </ItemBox>
